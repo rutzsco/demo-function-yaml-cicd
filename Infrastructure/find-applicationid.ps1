0d17e4585ac2ded97c8b723e3863e09af682d4f4
Scott Rutz <scrutz@microsoft.com>
2021-03-02 10:49:30 -0600

    cp

--- Infrastructure/find-applicationid.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Mandatory)]
-    $appName
-)
-$servicePrincipal = Get-AzADServicePrincipal  -SearchString $appName
-$appId = $servicePrincipal.ApplicationId
-
-Write-Output "Application id for application with name $appName is $appId"
-Write-Output ("##vso[task.setvariable variable=appId;]$appId")
